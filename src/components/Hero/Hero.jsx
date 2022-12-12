6bb4b989bd38d99950be68c3cdac73e22a339290
Amalia Cicala <cicala.amalia@gmail.com>
2022-12-12 11:32:42 +0100

    feat: animate hero carousel

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,9 +1,11 @@
-import { useState } from 'react';
+import { useState, useEffect, useRef } from 'react';
 
 import item from '../../locales/carousel.json';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faAngleRight } from '@fortawesome/free-solid-svg-icons';
+
+import { motion, AnimatePresence } from 'framer-motion';
 
 export default function Hero() {
     // slider
@@ -37,62 +39,107 @@ export default function Hero() {
         setIsHovering(false);
     };
 
+    // slider animation
+    const firstRender = useRef(true);
+
+    // don't animate on render
+    useEffect(() => {
+        if (firstRender.current) {
+            firstRender.current = false;
+            return;
+        }
+    });
+
+    const backgroundVariants = {
+        initial: { opacity: 0.9 },
+        animate: { opacity: 1 }
+    };
+
+    const titleVariantsEven = {
+        initial: { x: -50, opacity: 0.5 },
+        animate: { x: 0, opacity: 1 }
+    };
+
+    const titleVariantsOdd = {
+        initial: { x: 50, opacity: 0.5 },
+        animate: { x: 0, opacity: 1 }
+    };
+
     return (
         <>
-            <section
-                className="carousel-container"
-                key={item[currentIndex].id}
-                style={{
-                    backgroundImage: `url(${item[currentIndex].image})`
-                }}>
-                <section className="carousel-content">
-                    <h2>{item[currentIndex].heading}</h2>
-                    <h1>{item[currentIndex].title}</h1>
-                    <h4>{item[currentIndex].subtitle}</h4>
-                    <button>Discover</button>
-                </section>
+            <AnimatePresence initial="false">
+                <motion.div
+                    key={item[currentIndex].id}
+                    variants={firstRender.current ? {} : backgroundVariants}
+                    animate="animate"
+                    initial="initial"
+                    transition={{ duration: 0.5 }}
+                    className="carousel-container"
+                    style={{
+                        backgroundImage: `url(${item[currentIndex].image})`
+                    }}>
+                    <section className="carousel-content">
+                        <h2>{item[currentIndex].heading}</h2>
+                        <motion.h1
+                            key={item[currentIndex].id}
+                            variants={currentIndex % 2 == 0 ? titleVariantsEven : titleVariantsOdd}
+                            animate="animate"
+                            initial="initial"
+                            // exit="exit"
+                            transition={{
+                                duration: 0.4
+                            }}>
+                            {item[currentIndex].title}
+                        </motion.h1>
+                        <h4>{item[currentIndex].subtitle}</h4>
+                        <button>Discover</button>
+                    </section>
 
-                <section
-                    className="carousel-next-container"
-                    onMouseOver={handleMouseOver}
-                    onMouseOut={handleMouseOut}
-                    onClick={onSlideChange}
-                    key={item[nextIndex].id}>
-                    <div
-                        className="carousel-next-bg"
-                        style={{
-                            backgroundImage: `url(${item[nextIndex].image})`
-                        }}></div>
+                    <section
+                        className="carousel-next-container"
+                        onMouseOver={handleMouseOver}
+                        onMouseOut={handleMouseOut}
+                        onClick={onSlideChange}
+                        key={item[nextIndex].id}>
+                        <div
+                            className="carousel-next-bg"
+                            style={{
+                                backgroundImage: `url(${item[nextIndex].image})`
+                            }}></div>
 
-                    <div className="carousel-next-titles">
-                        <h1 className="carousel-next-title">{item[nextIndex].title}</h1>
-                        <div className="carousel-next-arrow">
-                            <h3>Next</h3>
-                            <FontAwesomeIcon icon={faAngleRight} style={{ marginLeft: '1rem' }} />
-                            <div
-                                className="arrow"
-                                style={{
-                                    visibility: isHovering ? 'visible' : 'hidden',
-                                    opacity: isHovering ? '1' : '0'
-                                }}>
+                        <div className="carousel-next-titles">
+                            <h1 className="carousel-next-title">{item[nextIndex].title}</h1>
+                            <div className="carousel-next-arrow">
+                                <h3>Next</h3>
                                 <FontAwesomeIcon
                                     icon={faAngleRight}
-                                    style={{ marginLeft: '0.2rem' }}
+                                    style={{ marginLeft: '1rem' }}
                                 />
-                                <FontAwesomeIcon
-                                    icon={faAngleRight}
-                                    style={{ marginLeft: '0.2rem' }}
-                                />
+                                <div
+                                    className="arrow"
+                                    style={{
+                                        visibility: isHovering ? 'visible' : 'hidden',
+                                        opacity: isHovering ? '1' : '0'
+                                    }}>
+                                    <FontAwesomeIcon
+                                        icon={faAngleRight}
+                                        style={{ marginLeft: '0.2rem' }}
+                                    />
+                                    <FontAwesomeIcon
+                                        icon={faAngleRight}
+                                        style={{ marginLeft: '0.2rem' }}
+                                    />
+                                </div>
                             </div>
                         </div>
-                    </div>
-                </section>
+                    </section>
 
-                <section className="carousel-next-mobile" onClick={onSlideChange}>
-                    <h3>Next</h3>
-                    <FontAwesomeIcon icon={faAngleRight} style={{ marginLeft: '1rem' }} />
-                </section>
-            </section>
+                    <section className="carousel-next-mobile" onClick={onSlideChange}>
+                        <h3>Next</h3>
+                        <FontAwesomeIcon icon={faAngleRight} style={{ marginLeft: '1rem' }} />
+                    </section>
+                </motion.div>
+            </AnimatePresence>
         </>
     );
 }
